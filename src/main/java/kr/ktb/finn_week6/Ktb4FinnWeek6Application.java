9e3b6ab993a42f0f00c33ddffd600f3a2a222fa2
정지호 <jeongjiho@jeongjihoui-MacBookPro.local>
2026-06-17 22:32:46 +0900

    init commit

---
+++ src/main/java/kr/ktb/finn_week6/Ktb4FinnWeek6Application.java
@@ -0,0 +1,13 @@
+package kr.ktb.finn_week6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ktb4FinnWeek6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ktb4FinnWeek6Application.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ktb/finn_week6/Ktb4FinnWeek6ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ktb.finn_week6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ktb4FinnWeek6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
