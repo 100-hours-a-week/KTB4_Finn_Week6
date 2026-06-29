480006c84cfc1ecdfabefcd4e87490b2fda9e2f0
정지호 <jeongjiho@jeongjihoui-MacBookPro.local>
2026-06-29 22:09:35 +0900

    done

--- src/main/java/kr/ktb/finn_week6/domain/post/dto/request/UpdatePostRequest.java
+++ src/main/java/kr/ktb/finn_week6/domain/post/dto/request/UpdatePostRequest.java
@@ -1,12 +1,12 @@
 package kr.ktb.finn_week6.domain.post.dto.request;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import kr.ktb.finn_week6.domain.post.dto.command.UpdatePostCommand;
 
 public record UpdatePostRequest(
         @NotBlank(message = "Title is required")
-        @Max(value = 26, message = "Title must be less than 26 characters")
+        @Size(max = 26, message = "Title must be less than 26 characters")
         String title,
         @NotBlank(message = "Content is required")
         String content,
